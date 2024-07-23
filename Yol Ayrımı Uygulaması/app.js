be05c61f44dbac41ca1fca3407d71cfd8e983229
ZelihaOrhann <zeliha.social1@gmail.com>
2024-07-23 19:38:24 +0300

    İkinci uygulama eklendi.

---
+++ Yol Ayrımı Uygulaması/app.js
@@ -0,0 +1,15 @@
+let secilenYol= prompt("Lütfen gitmek istediginiz yolu seçiniz: ");
+
+if(secilenYol==1){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else if(secilenYol==2){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else if(secilenYol==3){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else{
+    alert("Lütfen geçerli bir yol giriniz !!")
+}
+
