30f2813be041414c7846304801a077f1797efc2d
ZelihaOrhann <zeliha.social1@gmail.com>
2024-07-28 20:17:01 +0300

    On ikinci uygulama eklendi.

---
+++ Harf Sayısı Uygulaması/app.js
@@ -0,0 +1,16 @@
+let metin = "Şu anda Elazığ'da Javascript çalışmaktayım.";
+
+let harf = prompt("Harfi giriniz: ");
+
+let sonuc = bul(harf);
+alert("Harf sayısı: " + sonuc);
+
+function bul(harf){
+    let toplam = 0;
+    for(let i=0;i<metin.length;i++){
+        if(metin.charAt(i)==harf){
+            toplam+=1;
+        }
+    }
+    return toplam;
+}
